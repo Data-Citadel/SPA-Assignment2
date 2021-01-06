7faec4aaf5d150a5029d1a555527970f84f5f2b4
Bala Kavin Pon <42774004+balakavinpon@users.noreply.github.com>
2021-01-07 00:22:23 +0530

    initial commit

---
+++ producer.py
@@ -0,0 +1,83 @@
+import pandas as pd
+import random
+import json
+import time
+from kafka import KafkaProducer
+
+class Rand_Customer_Producer:
+
+    '''
+        bootstrap_servers - bootstap server name 
+        sample_range - Range of number of customer need to drwan from total set
+        radious  - distance of the customer from shop 
+                [0 - 1] - with in shop
+                [2 - 3] - very near to shop
+                [4 - 6] - near to shop
+                [7 - 10] - slightly away from shop
+        total_customers - total set we wanted to consider
+    '''
+
+    def __init__(self,bootstrap_servers,sample_range,radious,total_customers):
+        self.sample_range = sample_range
+        self.radious = radious
+        self.total_customers = total_customers
+        self. producer = self._get_kafka_producer(bootstrap_servers)
+
+    def _get_kafka_producer(self,bootstrap_servers):
+        # create kafka producer with json value serializer 
+        serializer = lambda v: json.dumps(v).encode('utf-8')
+        producer = KafkaProducer(bootstrap_servers=bootstrap_servers,value_serializer= serializer)
+        print("Producer running")
+        return producer
+
+
+    def _generateRamdomCustomer(self,sample_size=10,radious=10,total_size=200):
+        
+        cust_list = []
+        #Create total setof customers
+        total_set = range(1,total_size)
+
+        #select random n customer from total
+        selected_cust = random.sample(total_set,sample_size)
+
+        #add distance to customer
+        for i in selected_cust:
+            cust_list.append((i,random.randrange(0,radious)))
+        
+        return cust_list
+
+    def generate_and_publish_msg(self,topic_name):
+        # pick the number of custer we need to draw from the range given
+        size=random.choice(self.sample_range)
+        
+        #generate customer list with distance
+        customers = self._generateRamdomCustomer(sample_size=size,radious= self.radious, total_size =self.total_customers)
+        
+        # publish the customer with distance in kafka topic
+        for customer in customers:
+            self.producer.send( topic_name, {'id': customer[0], 'rad': customer[1]})
+        self.producer.flush()
+
+    def send_msg_in_loop(self, topic_name, interval=1, times=30):
+        # iterate for every 1 min (Interval) for next 30 times
+        for i in range(0,30):
+            print(f'publishing for ->{i} iteration')
+            self.generate_and_publish_msg(topic_name)
+            time.sleep(interval*60) # 
+
+        
+
+if __name__ == "__main__":
+    bootstrap_servers = 'localhost:9092'
+    topic_name = "test"
+    total_customers = 200
+    sample_range = range(15, 30)
+    radious = 10
+
+    producer = Rand_Customer_Producer(bootstrap_servers=bootstrap_servers,
+             sample_range=sample_range,
+             radious=radious,
+             total_customers=total_customers)
+    # time in min
+    producer.send_msg_in_loop(topic_name, 1 , 5)
+    
